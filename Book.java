3c97f8dbecb284fe2472d43517421592f1257a98
Cel Reyes <reyescelerinacr13@gmail.com>
2024-10-13 15:18:58 +0800

    Added comments to Book class

--- Book.java
+++ Book.java
@@ -6,6 +6,7 @@
 
 public class Book 
 {
+    // These are fields to store book detail...
     private String title;
     private int year;
     private String isbn;
@@ -13,6 +14,7 @@ public class Book
     private int edition;
     private Author[] authors;
 
+    // Constructor to initialise the book with default values :O
     public Book()
     {
         this.title = "None";
@@ -22,6 +24,7 @@ public class Book
         this.edition = 0;
     }
 
+    // These constructors have parameters for initialised, without the athors array :)
     public Book(String title, int year, String isbn, boolean ebook, int edition)
     {
         this.title = title;
@@ -31,62 +34,61 @@ public class Book
         this.edition = edition;
     }
 
-
+    // Getter and Setter for the title...
     public String getTitle() 
     {
         return title;
     }
-
     public void setTitle(String title)
     {
         this.title = title;
     }
 
+    // Getter and Setter for the published year...
     public int getYear()
     {
         return year;
     }
-
     public void setYear(int year)
     {
         this.year = year;
     }
 
+    // Getter and Setter for the ISBN...
     public String getIsbn()
     {
         return isbn;
     }
-
     public void setIsbn(String isbn)
     {
         this.isbn = isbn;
     }
 
+    // Getter and Setter to check that the book is an eBook...
     public boolean isEbook()
     {
         return ebook;
     }
-
     public void setEbook(boolean ebook)
     {
         this.ebook = ebook;
     }
 
+    // Getter and Setter for the edition...
     public int getEdition()
     {
         return edition;
     }
-
     public void setEdition(int edition)
     {
         this.edition = edition;
     }
 
+    // Getter and Setter for authors...
     public Author[] getAuthors()
     {
         return authors;
     }
-
     public void setAuthors(Author[] authors)
     {
         this.authors = authors;
